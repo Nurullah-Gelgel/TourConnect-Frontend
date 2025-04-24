d26631c3f9162aea7a4e8fbc6616eb6be0cdde87
Nurullah Gelgel <nurullahgelgel@ogr.eskisehir.edu.tr>
2025-04-25 02:50:55 +0300

    Room management added

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-//const BASE_URL = 'https://rahvan.onrender.com/'; // Backend URL'inizin doğru olduğundan emin olun
+const BASE_URL = 'https://rahvan.onrender.com/'; // Backend URL'inizin doğru olduğundan emin olun
 const BASE_URL = 'http://localhost:8080/'; // 
 //const BASE_URL = window.location.origin;  // Otomatik olarak doğru URL'i alır
 // Axios instance oluşturma
